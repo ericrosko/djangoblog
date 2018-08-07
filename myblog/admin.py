bfd7402b7eadca354af81476a86b59e0aa575dc3
Eric Rosko <eric_rosko@hotmail.com>
2018-08-06 17:28:46 -0700

    CategoryInline not working for assign 7

--- myblog/admin.py
+++ myblog/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # this affects the add category form, not the first list of all categories
+    fields = ('name', 'description')
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    # this affects the add post form, not the list of posts
+    fields = ('title','text', 'author', 'categories')
+    # fields = ('title','text', 'author', 'categories') I tried this
+    inlines = [
+        CategoryInline,
+    ]
+
+admin.site.register(Category, CategoryAdmin)   
+admin.site.register(Post, PostAdmin)
